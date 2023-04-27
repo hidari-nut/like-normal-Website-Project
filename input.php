0d9ed1d89b2d500667886efac06b6942d13c4aa8
yuveex <alvinsetiawan2003@gmail.com>
2023-04-27 17:22:51 +0700

    Cookie on setting.php update
    Cookie settings on setting.php added for all the user settings.

--- input.php
+++ input.php
@@ -4,27 +4,35 @@
 <!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
 <!--[if gt IE 8]>      <html class="no-js"> <!--<![endif]-->
 <html>
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <title></title>
-        <meta name="description" content="">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="stylesheet" href="">
-    </head>
-    <body>
 
-        <?php
-            session_start();
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title></title>
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="">
+</head>
 
-            if (isset($_POST["address-required"])) {
-                $address_required = $_POST["address-required"];
-                setcookie("address-required", $address_required, time() + 2628288, "/");
-                echo $_COOKIE["address-required"];
-            } else {
-            }
-        ?>
-    
-        <script src="" async defer></script>
-    </body>
+<body>
+
+    <?php
+    ini_set('display_errors', '1');
+    ini_set('display_startup_errors', '1');
+    error_reporting(E_ALL);
+    session_start();
+
+    if (isset($_POST["address-required"])) {
+        $address_required = $_POST["address-required"];
+        setcookie("address-required", $address_required, time() + 2628288, "/");
+        echo $_COOKIE["address-required"];
+    } else {
+    }
+
+
+    ?>
+
+    <script src="" async defer></script>
+</body>
+
 </html>
