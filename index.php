8fc0908a9ac6a2600612e75b2b86a9db2ecbb85f
hidari-nut <madevis02@gmail.com>
2023-04-28 23:10:49 +0700

    Updated

--- index.php
+++ index.php
@@ -4,25 +4,33 @@
 <!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
 <!--[if gt IE 8]>      <html class="no-js"> <!--<![endif]-->
 <html>
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <title></title>
-        <meta name="description" content="">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="stylesheet" href="">
-    </head>
-    <body>
-        
-        <h1>Welcome!</h1>
 
-        <form>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title></title>
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="">
+</head>
+
+
+<body>
+
+    <h1>Welcome!</h1>
+
+    <!-- <form>
             <input type="button" onclick="window.location.href='setting.php';" value="Settings" />
             <input type="button" onclick="window.location.href='input.php';" value="Input" />
             <input type="button" onclick="window.location.href='display.php';" value="Display" />
-        </form>
-        
-        
-        <script src="" async defer></script>
-    </body>
+        </form> -->
+
+    <a href = "setting.php"> Settings </a> <br><br>
+    <a href = "input.php"> Input </a><br><br>
+    <a href = "display.php"> Display </a>
+
+
+    <script src="" async defer></script>
+</body>
+
 </html>
